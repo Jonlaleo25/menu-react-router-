8b9412ac515287d18c8381236857ba18d64e3e3f
Jonlaleo25 <jonathanar@librerialeo.com.mx>
2018-11-01 12:15:10 -0600

    Practica de menu de router

---
+++ src/Component/Writers/Writer/index.js
@@ -0,0 +1,16 @@
+import React, {  Fragment } from 'react';
+
+export default  ({ match, id, name, description,image}) =>
+
+// console.log(match) ||
+<Fragment>
+
+        {id}
+
+        {name}
+
+        {description}
+
+        {image}
+
+</Fragment>
